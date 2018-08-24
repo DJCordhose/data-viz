0bd3237effd7b1dd93c71a705896c6b1be39677d
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2018-08-24 15:23:05 +0200

    batch of changes for matplotlib unit

---
+++ notebooks/exercise/matplotlib-hello-world.py
@@ -0,0 +1,10 @@
+# Create a scatter plot that has 4 data points
+# * they shall be displayed as displayed as triangles
+# * each point should have a different color
+# * zoom into the plot so that you only see the two bottom left values
+
+plt.scatter([0.3, 3.8, 1.2, 2.5], [11, 25, 9, 26])
+plt.scatter?
+# plt.colors?
+# plt.xlim?
+# plt.ylim?
